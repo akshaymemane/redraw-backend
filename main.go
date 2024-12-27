2523b3e8b298f028ac9833c689b4fa06840be296
Akshay Memane <akshaymemane29@gmail.com>
2024-12-28 01:10:34 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to upgrade connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Add new client
+	clients[conn] = true
+	defer delete(clients, conn)
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			return
+		}
+		// Broadcast to all clients
+		for client := range clients {
+			if err := client.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println("Error sending message to client:", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnection)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
